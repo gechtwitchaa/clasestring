fafd45812475cd46f87af73e0743b107731a856f
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-23 12:48:22 +0100

    .

---
+++ funcionesespecificas.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string cadena = "Hola, mundo!";
+
+    std::string subcadena = "mundo";
+    size_t indice = cadena.find(subcadena);
