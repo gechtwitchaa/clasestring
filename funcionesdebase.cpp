9935b8d57eea65134ebb03ecbbd708a079804382
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-23 12:36:49 +0100

    .

--- funcionesdebase.cpp
+++ funcionesdebase.cpp
@@ -4,4 +4,6 @@
 int main() {
     std::string sentence = "OpenAI is a leading AI research lab.";
     std::cout << "Tamaño de la cadena: " << sentence.size() << std::endl;
- 
+    std::cout << "¿Está la cadena vacía? " << (sentence.empty() ? "Sí" : "No") << std::endl;
+    return 0;
+}
