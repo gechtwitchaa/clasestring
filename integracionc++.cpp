3db0d3f3c230a4397499fd54720745d0b5ba7f01
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-23 12:42:52 +0100

    .

--- integracionc++.cpp
+++ integracionc++.cpp
@@ -6,4 +6,9 @@ int main() {
     std::string cadena1 = "Hola, ";
     std::string cadena2 = "mundo!";
 
+    std::string resultado = cadena1 + cadena2;
 
+    std::cout << "La cadena resultante es: " << resultado << std::endl;
+
+    return 0;
+}
