147c407414c43df9e2c89a984e93ca748e7b6cad
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-23 12:48:46 +0100

    .

--- funcionesespecificas.cpp
+++ funcionesespecificas.cpp
@@ -7,3 +7,7 @@ int main() {
 
     std::string subcadena = "mundo";
     size_t indice = cadena.find(subcadena);
+    if (indice != std::string::npos) {
+        std::cout << "La subcadena '" << subcadena << "' comienza en el índice: " << indice << std::endl;
+
+
